aa61080077a3e97dbe958f07c57170977eb6a31c
ishii-taa <toishitagarg@gmail.com>
2025-11-15 19:07:47 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import MapView from "./pages/MapView";
+import Alerts from "./pages/Alerts";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav className="bg-blue-600 p-4 text-white flex gap-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/map" className="hover:underline">Map</Link>
+        <Link to="/alerts" className="hover:underline">Alerts</Link>
+      </nav>
+
+      {/* Page content */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<MapView />} />
+        <Route path="/alerts" element={<Alerts />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
 
